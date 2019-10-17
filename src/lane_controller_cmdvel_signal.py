69436129ada3283b1472ccae5780d662a5fa6fe2
Alberto <alberto.dionigi95@gmail.com>
2019-10-17 08:44:44 +0000

    irrobustita la detection

--- src/lane_controller_cmdvel_signal.py
+++ src/lane_controller_cmdvel_signal.py
@@ -9,6 +9,7 @@ from geometry_msgs.msg import Twist
 from std_msgs.msg import Int32
 from master_node.msg import *
 from master_node.srv import *
+from fiducial_msgs.msg import *
 
 #set id node for master node priority
 id_node = "lane"
@@ -31,6 +32,9 @@ followmessage = Follow()
 followmessage.id = id_node
 
 lock = False
+
+last_x_fiducial = None
+last_id_fiducial = None
 
 pub = rospy.Publisher("follow_topic",Follow,queue_size=1)
 request_lock_service = rospy.ServiceProxy('request_lock', RequestLockService)
@@ -79,10 +83,22 @@ def setSpeed(x_speed,z_speed):
 		pub.publish(followmessage)
 
 def callback(data):
+	global last_x_fiducial
 
 	error = data.data
 	x_speed = 0.1
+	x_semaforo_stop = 0
+	x_semaforo_detect = 1
+	K_x = x_speed/(x_semaforo_detect-x_semaforo_stop)
+
 	z_thr = 0.1
+	
+	if(last_x_fiducial != None):
+		if(last_x_fiducial < x_semaforo_detect):
+			error_x = last_x_fiducial - x_semaforo_stop
+			x_speed = error_x * K_x
+			print("x_speed: ",x_speed,"Kx",K_x)
+
 
 #	PID = calculatePID(error,0.5,0.0005,0.005)
 	PID = calculatePID(error,0.0025,0.0001,0)
@@ -95,6 +111,32 @@ def callback(data):
 
 	else:
 		setSpeed(x_speed,PID)
+
+
+def callback_fiducial(data):
+	global last_id_fiducial, last_x_fiducial, lock
+
+	transform_array = data.transforms
+
+	if transform_array == []:
+		if(not lock):
+			last_id_fiducial = None
+			last_x_fiducial = None
+		#print('non vedo nulla')
+	else:
+		if(lock):
+			transform = transform_array[0].transform
+			vector = transform.translation
+
+			current_id = transform_array[0].fiducial_id
+			if(current_id < 4):
+				last_id_fiducial = current_id
+				last_x_fiducial = vector.z
+		else:
+			last_id_fiducial = None
+			last_x_fiducial = None
+
+
 
 def releaseLock():
     global id_node, lock
@@ -133,6 +175,7 @@ def lane_controller():
 	rospy.init_node('lane_controller', anonymous=True)
 	rospy.Subscriber("lock_shared",Lock,checkMessage) #Subscribe
 	rospy.Subscriber('lane_detection', Int32, requestLock)
+	rospy.Subscriber("fiducial_transforms", FiducialTransformArray, callback_fiducial)
 
         try:
             rospy.on_shutdown(releaseLock)
